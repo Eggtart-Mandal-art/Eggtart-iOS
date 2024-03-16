12ea76f4ced6cc99586360123043158405c66a04
이준영 <junlight94@gmail.com>
2024-03-17 03:10:30 +0900

    chore. git ignore

--- Eggtart/Tuist/Package.swift
+++
@@ -1,9 +0,0 @@
-// swift-tools-version:5.9
-import PackageDescription
-
-let package = Package(
-    name: "EggtartPackage",
-    dependencies: [
-        .package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: "15.0.3"))
-    ]
-)
